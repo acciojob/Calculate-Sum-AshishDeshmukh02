1649364286407850dcf85347b9be280eaecf3c45
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-20 12:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function calculateSum() {
+			// Get the input values
+			let n1 = parseInt(document.getElementById("n1").value);
+			let n2 = parseInt(document.getElementById("n2").value);
+
+			// Calculate the sum
+			let sum = n1 + n2;
+
+			// Display the sum
+			document.getElementById("sum").innerHTML = "The sum is: " + sum;
+		}
